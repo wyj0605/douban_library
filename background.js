0d16f06f23eae45c7a1e26b3ccf8d7d1a9b95d15
王义军 <wyj0605@gmail.com>
2024-03-15 15:47:11 +0800

    修改backupground.js

--- background.js
+++ background.js
@@ -1,7 +1,4 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-
-
     if (request.action === "getProvinceStatus") {
         // 读取存储的省份状态数据
         chrome.storage.local.get(null, function(items) {
@@ -13,13 +10,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 }); 
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === 'plugin_started') {
-      // 插件开始运行时设置徽章
-      chrome.browserAction.setBadgeText({ text: 'ON' });
-      chrome.browserAction.setBadgeBackgroundColor({ color: '#00FF00' });
-    } else if (message.action === 'plugin_stopped') {
-      // 插件停止运行时清除徽章
-      chrome.browserAction.setBadgeText({ text: '' });
-    }
-  });
