fda57b09b1efee7ee3c3fd6533e3bbe040c1be41
王义军 <wyj0605@gmail.com>
2024-03-27 13:34:16 +0800

    支持广西壮族自治区图书馆

--- provinces.js
+++ provinces.js
@@ -39,5 +39,6 @@ const provinces = [
   { code: "38", name: "上海浦东" },
   { code: "39", name: "福泉市" },
   { code: "40", name: "来宾市" },
-  { code: "41", name: "重庆市" }
+  { code: "41", name: "重庆市" },
+  { code: "42", name: "广西壮族自治区" }
 ];
