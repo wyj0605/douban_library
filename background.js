991758868b68482fdcf22c066348d0a94a1831d0
wyj0605 <oncwnuDJ64Cl59J2xas9z5jhWpZ4@git.weixin.qq.com>
2024-06-21 16:38:06 +0800

    新增加南昌市图书馆、贵阳市图书馆

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-const city_info_map = {
-    "1": "http://1.189.39.9:2333",
-
-};
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "getProvinceStatus") {
@@ -15,31 +11,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 }); 
 
-chrome.runtime.onInstalled.addListener(() => {
-  
-    chrome.contextMenus.create({
-
-      id: "searchBaidu",
-      title: "图书馆查询助手：%s",
-      contexts: ["selection"]
-    });
-  });
-  
-// 监听右键菜单点击事件
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "searchLibrary" && info.selectionText) {
-        chrome.storage.local.get(["getProvinceStatus"], function(items) {
-            const getProvinceStatus = items.code;
-            const libraryInfo = city_info_map[getProvinceStatus];
-
-            if (libraryInfo) {
-                const [province, baseUrl] = libraryInfo;
-                const query = encodeURIComponent(info.selectionText);
-                const url = `${baseUrl}?searchWay=title&q=${query}&searchSource=reader&scWay=dim&marcformat=&sortWay=score&sortOrder=desc&startPubdate=&endPubdate=&rows=10&hasholding=1`;
-                chrome.tabs.create({ url: url });
-            } else {
-                console.error("Invalid library code or library not found.");
-            }
-        });
-    }
-});
